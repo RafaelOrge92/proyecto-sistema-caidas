e1f67e880e1d01a6a9e55da1927d845a902e74a4
pablonieto1305-web <pablonieto1305@gmail.com>
2026-02-03 12:10:29 +0100

    feat: logica formularios usuario y dispositivos

---
+++ fall-detection-frontend/src/types/index.ts
@@ -0,0 +1,18 @@
+// src/types/index.ts
+
+export type UserRole = 'ADMIN' | 'CUIDADOR' | 'USUARIO';
+
+export interface User {
+  id: string;        // UUID o número
+  fullName: string;
+  email: string;
+  role: UserRole;
+  isActive: boolean; // Para el Soft Delete
+}
+
+export interface Device {
+  id: string;         // ID interno de la BD
+  deviceId: string;   // El ID físico del ESP32 (ej: ESP32-001) [cite: 56]
+  alias: string;      // Nombre amigable
+  assignedUserId: string | null; // ID del usuario asociado
+}
