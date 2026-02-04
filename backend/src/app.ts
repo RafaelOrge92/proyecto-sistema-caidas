5bce7bebb3a79f4f26290b2fc5ce6b8634242447
ReneeRodrig <renerodrigalonso@gmail.com>
2026-02-04 22:32:59 +0100

    listen y creacion de la app separados en main y app respectivamente

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,5 @@ app.get('/health', (req: Request, res: Response) => {
     res.send("ok: True")
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+
+export default app
